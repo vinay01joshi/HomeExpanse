a4fd798b6c035d1a28a61238b041a269a4353a23
vinay01joshi <vinay01joshi@gmail.com>
2017-06-12 16:08:18 +0530

    updating gulp file using more plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,36 @@
 var gulp  = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var util = require('gulp-util');
+var gulpprint = require('gulp-print');
+var gulpif = require('gulp-if');
+var args = require('yargs').argv;
+
 gulp.task('vet',function(){
+    log('Analyzing source with JSHint and JSCS');
     gulp.src([       
         './server/**/*.js',
         './*.js',
         './public/js/**/*.js'
     ])
+    .pipe(gulpif(args.verbose,gulpprint()))
     .pipe(jscs())
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish',{verbose: true}));
+    //.pipe(jshint.reporter('fail'));
 
 });
+
+
+/* reusable function */
+function log(msg){
+    if(typeof(msg) == 'object'){
+        for(var item in msg){
+            if(msg.hasOwnProperty(item)){
+                util.log(util.colors.blue(msg[item]));
+            }
+        }
+    }else{
+        util.log(util.colors.blue(msg));
+    }
+}
